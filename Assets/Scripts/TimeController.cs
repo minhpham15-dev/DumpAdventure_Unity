2f8886157d9822fc097eef09b763bcc34065c7cb
Diep Quynh <remilia.1505@gmail.com>
2022-06-05 22:25:50 +0700

    Fix time counting
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

--- Assets/Scripts/TimeController.cs
+++ Assets/Scripts/TimeController.cs
@@ -9,12 +9,21 @@ public class TimeController : MonoBehaviour
     private int second = 0;
     private int min = 0;
 
+    private string secondString = "";
+    private string minString = "";
+
+    private float totalTime = 0;
+
     [SerializeField] private Text time;
 
     private void Update()
     {
-        min = Mathf.RoundToInt(Time.deltaTime / 60);
-        second = Mathf.RoundToInt(Time.deltaTime % 60);
-        time.text = string.Format("Time: {0}:{1}", min.ToString(), second.ToString());
+        totalTime += Time.deltaTime;
+        min = Mathf.RoundToInt(totalTime / 60);
+        second = Mathf.RoundToInt(totalTime % 60);
+
+        minString = min < 10 ? String.Format("0{0}", min) : min.ToString();
+        secondString = second < 10 ? String.Format("0{0}", second) : second.ToString();
+        time.text = string.Format("Time: {0}:{1}", minString, secondString);
     }
 }
