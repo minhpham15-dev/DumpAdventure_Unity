301fe8dbb921df1e2142fc07ddf57d03a7d3afe1
Diep Quynh <remilia.1505@gmail.com>
2022-06-05 18:54:44 +0700

    Add ScoreManager class
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts
+{
+    public class ScoreManager
+    {
+        private static ScoreManager instance;
+
+        public static ScoreManager Instance { 
+            get { 
+                if (instance == null)
+                    instance = new ScoreManager();
+
+                return instance;
+            }
+        }
+    }
+}
